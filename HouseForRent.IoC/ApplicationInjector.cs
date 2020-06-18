34852656eb7f2a8dca209efac7224f4296e62b2e
Thái Lê Đình <lethai1996@hotmail.com>
2020-06-18 14:07:37 +0700

    add configs to IoC

---
+++ HouseForRent.IoC/ApplicationInjector.cs
@@ -0,0 +1,31 @@
+﻿using AutoMapper;
+using HouseForRent.Application.MappingConfigs;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+
+namespace HouseForRent.IoC
+{
+    public class ApplicationInjector
+    {
+        public static void Register(IServiceCollection services)
+        {
+            //Application
+            var mappingConfig = AutoMapping.RegisterMappings();
+            IMapper mapper = mappingConfig.CreateMapper();
+            services.AddSingleton(mapper);
+            services.AddScoped<IMapper>(sp => new Mapper(sp.GetRequiredService<IConfigurationProvider>(), sp.GetService));
+
+            // Domain - Commands
+            var serviceDependencies = Assembly.Load("HouseForRent.Application").GetTypes().Where(x => x.IsClass && x.Name.EndsWith("Service"));
+            foreach (var serviceDependency in serviceDependencies)
+            {
+                var type = serviceDependency.GetInterfaces().FirstOrDefault();
+                services.AddScoped(type, serviceDependency);
+            }
+        }
+    }
+}
---
+++ HouseForRent.IoC/InfrasstructureInjector.cs
@@ -0,0 +1,23 @@
+﻿using DomainCore.UnitOfWork;
+using HouseForRent.Data.EF;
+using HouseForRent.DomainCore.Repository;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HouseForRent.IoC
+{
+    public class InfrasstructureInjector
+    {
+        public static void Register(IServiceCollection services)
+        {
+            services.AddDbContext<HouseForRentDbContext>(ServiceLifetime.Transient);
+
+            services.AddScoped(typeof(IUnitOfWork), typeof(UnitOfWork));
+
+            services.AddScoped(typeof(IBaseRepository<>), typeof(BaseRepository<>));
+
+        }
+    }
+}
